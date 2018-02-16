a73230c3cce8cf20e9f794b66121e081b8717085
murisimov <murisimov@gmail.com>
2018-02-15 19:51:18 -0800

    Zero deviation fix

--- helpers/check.py
+++ helpers/check.py
@@ -1,11 +1,11 @@
 def is_target(target_rgb, current_rgb, deviation):
     t_r, t_g, t_b = target_rgb
     r_lower = t_r - deviation if t_r - deviation > 0 else t_r
-    r_upper = t_r + deviation if t_r + deviation < 256 else t_r + 1
+    r_upper = t_r + deviation + 1 if t_r + deviation < 256 else t_r + 1
     g_lower = t_g - deviation if t_g - deviation > 0 else t_g
-    g_upper = t_g + deviation if t_g + deviation < 256 else t_g + 1
+    g_upper = t_g + deviation + 1 if t_g + deviation < 256 else t_g + 1
     b_lower = t_b - deviation if t_b - deviation > 0 else t_b
-    b_upper = t_b + deviation if t_b + deviation < 256 else t_b + 1
+    b_upper = t_b + deviation + 1 if t_b + deviation < 256 else t_b + 1
 
     r, g, b = current_rgb
 
